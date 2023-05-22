a910a0bc053ee8b63859563adc32431ae4ca682b
Thacio Santos de Santana <59703956+ThacioSantana@users.noreply.github.com>
2023-05-22 18:42:56 -0300

    teste

--- Teste/js/script-cadastro.js
+++ Teste/js/script-cadastro.js
@@ -1,15 +1,28 @@
-function registerRegularClient(name, email, password) {
-    // Lógica para registrar um cliente regular
-    // ...
-    console.log("Cliente Regular Registrado");
+function registerRegularClient(name, email, password, type) {
+  // Lógica para registrar um cliente regular
+  if (type === 'CPF') {
+    // Lógica específica para clientes pessoa física (CPF)
+    console.log("Cliente Regular (CPF) Registrado");
+  } else if (type === 'CNPJ') {
+    // Lógica específica para clientes varejistas (CNPJ)
+    console.log("Cliente Regular (CNPJ) Registrado");
+  } else {
+    console.log("Tipo de cliente inválido");
   }
-  
-  function registerPremiumClient(name, email, password, membershipLevel) {
-    // Lógica para registrar um cliente premium
-    // ...
-    console.log("Cliente Premium Registrado");
+}
+
+function registerPremiumClient(name, email, password, membershipLevel, type) {
+  // Lógica para registrar um cliente premium
+  if (type === 'CPF') {
+    // Lógica específica para clientes pessoa física (CPF)
+    console.log("Cliente Premium (CPF) Registrado");
+  } else if (type === 'CNPJ') {
+    // Lógica específica para clientes varejistas (CNPJ)
+    console.log("Cliente Premium (CNPJ) Registrado");
+  } else {
+    console.log("Tipo de cliente inválido");
   }
-  
-  registerRegularClient("John Doe", "john@example.com", "password123");
-  registerPremiumClient("Jane Smith", "jane@example.com", "password456", "Gold");
-  
+}
+
+registerRegularClient("John Doe", "john@example.com", "password123", "CPF");
+registerPremiumClient("Jane Smith", "jane@example.com", "password456", "Gold", "CNPJ");
